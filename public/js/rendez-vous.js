1365a0c7e4cb9547a522699837153d33e56a0556
Adel LATIBI <alatibi@campus-eni.fr>
2022-10-24 13:08:21 +0200

    update

--- public/js/rendez-vous.js
+++ public/js/rendez-vous.js
@@ -1,23 +1,25 @@
-       //$("#selector").flatpickr().l10n.weekdays.longhand = ['Dimanche', 'Lun', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
-       $("#selector").flatpickr().l10n.weekdays.shorthand = ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa'];
-       $("#selector").flatpickr().l10n.months.longhand =['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
-       console.log($("#selector").flatpickr());
-       function weekend(date) {
-           return (date.getDay() === 0);
-       }
-       jQuery(document).ready(function($){
-           
-           $("#selector").flatpickr({                
-               dateFormat: "Y-m-d",
-               minDate: "today",
-               disable: [weekend],
-           });  
-           
-           $("#selector").change(function(){
-               var date = $(this).val();
-               var medecin = $("#rendez_vous_medecin").val();
-               $.get("/rendez-vous/get/"+date+"/"+medecin,(data)=>{
-                   $("#ava").html(data);
-               });
-           })
-       });
+function weekend(date) {
+    return (date.getDay() === 0);
+}
+jQuery(document).ready(function($){
+
+    $("#ava").on("click","table .abled",function(){
+        $("table td").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#selector").flatpickr({                
+        dateFormat: "Y-m-d",
+        minDate: "today",
+        locale:'fr',
+        disable: [weekend],
+    });  
+    
+    $("#selector").change(function(){
+        var date = $(this).val();
+        var medecin = $("#rendez_vous_medecin").val();
+        $.get("/rendez-vous/get/"+date+"/"+medecin,(data)=>{
+            $("#ava").html(data);
+        });
+    })
+});
